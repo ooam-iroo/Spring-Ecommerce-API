b2b5ac4e35271a176d232c3ff2b77d43606972b6
amir <amyrmdi99@gmail.com>
2026-08-30 15:57:06 +0330

    feat(security): configure public product and category endpoints

--- src/main/java/com/example/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/example/ecommerce/config/SecurityConfig.java
@@ -4,8 +4,10 @@ import com.example.ecommerce.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,6 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -37,6 +40,13 @@ public class SecurityConfig {
                                 .requestMatchers(
                                         "/api/v1/auth/**"
                                 ).permitAll()
+
+                                .requestMatchers(
+                                        HttpMethod.GET,
+                                        "/api/v1/products/**",
+                                        "/api/v1/categories/**"
+                                ).permitAll()
+
                                 .anyRequest().authenticated()
                 )
                 .addFilterBefore(
